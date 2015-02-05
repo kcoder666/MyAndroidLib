dea67fcd7b1b0e5e5fa0c2c6fa42c51c1a2707d8
khoa.tran <khoa.tran@notabasement.com>
2015-02-05 17:25:13 +0700

    Upload repo structure and files

---
+++ commonlibrary/src/main/java/me/tvkkpt/commonlibrary/Utils.java
@@ -0,0 +1,14 @@
+package me.tvkkpt.commonlibrary;
+
+/**
+ * Author: Khoa Tran.
+ * Email: khoa.tran@notabasement.com.
+ * Date: 2/5/15.
+ */
+public class Utils {
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+}
